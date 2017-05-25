c77d78f523e200da83ce42dd2555a4264f91c42a
cookieshake <cookieshake@hotmail.com>
2017-05-25 13:04:57 +0900

    package files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+
+setup(
+    name='pyokt',
+    version='0.1.1',
+    description='open-korean-text wrapper for python',
+
+    url='https://github.com/cookieshake/pyokt',
+
+    author='cookieshake',
+    author_email='cookieshake@github.com',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+    ],
+
+    # What does your project relate to?
+    keywords='open-korean-text korean text tokenize',
+    packages=['pyokt'],
+    install_requires=['jpype1'],
+)
